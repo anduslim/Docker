b86501394e5e190989361a6141ef9ae333984e13
Andus <yuncai83@gmail.com>
2015-01-15 22:23:51 -0500

    Updated tinymosquitto script and Dockerfile

--- tiny-mosquitto/buildimage.sh
+++ tiny-mosquitto/buildimage.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 export IMAGENAME=tinymosquitto
-export BUILDROOTVER=2014.11
-
+export BUILDROOTVER=2014.05
 
 mkdir ~/src
 cd ~/src
 
 wget http://buildroot.uclibc.org/downloads/buildroot-$BUILDROOTVER.tar.gz
 tar -xzvf buildroot-$BUILDROOTVER.tar.gz
-git clone https://github.com/lexlapax/Dockerfiles
+git clone https://github.com/snsx/Docker.git Dockerfiles
 cp -r Dockerfiles/tiny-mosquitto/buildroot/package/mosquitto buildroot-$BUILDROOTVER/package/
 cp Dockerfiles/tiny-mosquitto/buildroot/buildroot.config buildroot-$BUILDROOTVER/.config
 cd buildroot-$BUILDROOTVER
-make all
+make menuconfig
+make
 # wait a really long time while it builds everything including the toolchain
 cd output/images
 mkdir extra extra/sbin extra/etc
