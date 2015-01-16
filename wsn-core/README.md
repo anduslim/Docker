518717e5d23b423185297ed1305c434ceb2168c5
Andus <yuncai83@gmail.com>
2015-01-15 21:15:36 -0500

    Initial commit with wsn-core

---
+++ wsn-core/README.md
@@ -0,0 +1,10 @@
+A minimal runtime only docker encapsulation of the mosquitto mqtt broker
+
+Currently uses http://mosquitto.org/files/source/mosquitto-1.3.5.tar.gz
+
+This is built using buildroot v14.05 (see http://buildroot.uclibc.org/) in order to make the image small. It has a small size of 20 MB.
+
+The docker image was built with instructions from
+
+http://blog.docker.io/2013/06/create-light-weight-docker-containers-buildroot/
+
---
+++ wsn-core/init.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export WSN_DIR="$HOME/wsn-core"
+
+#if [ ! -d "$WSN_DIR" ]; then
+#  echo "Creating WSN directory in $WSN_DIR"
+#  mkdir -p $WSN_DIR
+#fi
+
+MNT_PATH=`docker inspect -f='{{(index .Volumes "/root/wsn-core")}}' wsn`
+
+eval "ln -sf $MNT_PATH $WSN_DIR"
+
+echo "Creating symbolic link to $WSN_DIR."
+
+while true; do
+    echo "What is your Github username?"
+    read git_user
+    while true; do
+        read -p "A new git remote, git@github.com:$git_user/wsn-core.git will be added. Continue? (Y/N)" yn
+        case $yn in
+            [Yy]* ) ( cd $WSN_DIR && git remote add origin git@github.com:$git_user/wsn-core.git ); break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer Y or N.";;
+        esac
+    done
+    echo "yn is $yn"
+    if [[ $yn =~ ^[Yy]$ ]];
+    then
+        break
+    fi
+done
+
+while true; do
+    eval "cd $WSN_DIR && git pull origin integration"
+    
+    if [ $? -eq 1 ]; then
+        if [ ! -f ~/.ssh/*.pub ]; then
+            echo "ssh key doesnt exist. Creating ..."
+            ( ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa )
+        else
+            echo "Please add your ~/.ssh/id_rsa.pub key into your github account."
+            break
+        fi
+    else
+        break
+    fi
+done
+echo "Result is $?"
+
+
